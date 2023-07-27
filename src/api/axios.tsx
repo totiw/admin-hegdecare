a8e77caf173b8cdddff7bb44e2e0801122f4e618
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-07-27 18:18:39 +0700

    feat(api): change api endpoint;

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://api.hedgecare.ca",
-  baseURL: "https://address.serbadiskon.id",
+  baseURL: "https://api.hedgecare.ca",
+  // baseURL: "https://address.serbadiskon.id",
 });
